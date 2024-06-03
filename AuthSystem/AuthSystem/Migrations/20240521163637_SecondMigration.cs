3f2143f901101e218e7a97587127a08264bc6c0a
BurduliGiorgi <burdulig25@gmail.com>
2024-06-03 19:35:26 +0400

    Add-Identity

---
+++ AuthSystem/AuthSystem/Migrations/20240521163637_SecondMigration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AuthSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "1a65d669-fe69-4b1f-820b-fa2fce596e0a", null, "seller", "seller" },
+                    { "d8501308-5468-4849-9516-a47af46b91f5", null, "client", "client" },
+                    { "dfff5761-17fc-4e27-b0b1-4e12075043e0", null, "admin", "admin" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1a65d669-fe69-4b1f-820b-fa2fce596e0a");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "d8501308-5468-4849-9516-a47af46b91f5");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "dfff5761-17fc-4e27-b0b1-4e12075043e0");
+        }
+    }
+}
