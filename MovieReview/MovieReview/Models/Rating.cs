96651f292bebd326092c4cbdaccbcbb22a129a7a
BurduliGiorgi <burdulig25@gmail.com>
2024-06-11 19:30:38 +0400

    fill-models

--- MovieReview/MovieReview/Models/Rating.cs
+++ MovieReview/MovieReview/Models/Rating.cs
@@ -1,6 +1,16 @@
-﻿namespace MovieReview.Models
+﻿
+namespace MovieReview.Models
 {
     public class Rating
     {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int MovieId { get; set; }
+        public int RatingValue { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public ApplicationUser User { get; set; }
+        public Movie Movie { get; set; }
     }
 }
+
--- MovieReview/MovieReview/Models/Review.cs
+++ MovieReview/MovieReview/Models/Review.cs
@@ -2,5 +2,17 @@
 {
     public class Review
     {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int MovieId { get; set; }
+        public int Rating { get; set; }
+        public string ReviewText { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        // Navigation properties
+        public ApplicationUser User { get; set; }
+        public Movie Movie { get; set; }
+
     }
 }
+
